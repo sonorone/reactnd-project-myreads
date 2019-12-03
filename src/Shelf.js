2120b33f67369ec8912a5c45757b75dc354aa9bb
Damian Murawiecki <dmurawiecki@myseneca.ca>
2019-12-02 22:43:12 -0500

    changed to function component

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,30 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import BookCard from "./BookCard";
-class Shelf extends Component {
-  state = {};
-
-  handleShelfChange = (book, shelf) => {
-    this.props.onShelfChange(book, shelf);
+function Shelf(props) {
+  const handleShelfChange = (book, shelf) => {
+    props.onShelfChange(book, shelf);
   };
 
-  render() {
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.name}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {this.props.books.map(book => (
-              <BookCard
-                key={book.id}
-                book={book}
-                onShelfChange={this.handleShelfChange}
-              />
-            ))}
-          </ol>
-        </div>
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.name}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.books.map(book => (
+            <BookCard
+              key={book.id}
+              book={book}
+              onShelfChange={handleShelfChange}
+            />
+          ))}
+        </ol>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Shelf;
